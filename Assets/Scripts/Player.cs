27cc01b2b5c52cf589ea53781e9625f2192ec30e
luotao <luotaoyeah@gmail.com>
2022-02-23 23:20:39 +0800

    feat: TextMeshProUGUI
    https://www.bilibili.com/video/BV18U4y187ug?p=38

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,12 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody myRigidBody;
+
+    public TextMeshProUGUI Text;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,7 @@ public class Player : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        Text.text = DateTime.Now.ToString();
 
         myRigidBody.AddForce(new Vector3(h, 0, v) * 2);
     }
